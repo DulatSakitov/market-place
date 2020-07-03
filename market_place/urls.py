6abfde44a8e6390f928bb7382d4e93098718963d
Dulat <dulat.django@gmail.com>
2020-07-03 12:45:32 +0600

    static urls

--- market_place/urls.py
+++ market_place/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
 from market.urls import urlpatterns as market_urls
 
 urlpatterns = [
@@ -40,4 +42,4 @@ schema_view = get_schema_view(
 
 urlpatterns += [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
